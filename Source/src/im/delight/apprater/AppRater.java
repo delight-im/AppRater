1e82c9ff9b75d766b7f8330d49d3ece87e6adf25
Marco <marco1@live.de>
2016-01-28 15:22:57 +0100

    Update copyright notices

--- Source/src/im/delight/apprater/AppRater.java
+++ Source/src/im/delight/apprater/AppRater.java
@@ -1,7 +1,7 @@
 package im.delight.apprater;
 
-/**
- * Copyright 2013 www.delight.im <info@delight.im>
+/*
+ * Copyright (c) delight.im <info@delight.im>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
